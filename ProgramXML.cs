33451d6ec5ef9fa8d1b659abb2cbf326b44cf547
mariami3 <144897983+mariami3@users.noreply.github.com>
2023-11-09 22:43:24 +0300

    Add files via upload

---
+++ ProgramXML.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+
+
+Student student =  new Student();
+student.Name = "Саша";
+student.Age = 25;
+student.City = "Москва";
+student.Speciality = "Медицинский";
+
+XmlSerializer xml = new XmlSerializer(typeof(Student));
+using(FileStream fs = new FileStream("Саша.xml", FileMode.OpenOrCreate))
+{
+    xml.Serialize(fs, student);
+}
+
+foreach (var item in student)
+{
+    Console.WriteLine(item.Name);
+    Console.WriteLine(item.Age);
+}
+
+Console.WriteLine("Сохранение в файл");
+Path = Console.ReadLine();
+
+if (Path.EndsWith(" .xml"))
+{
+    XmlSerializer xml = new XmlSerializer(typeof(Student));
+    using (FileStream fs = new FileStream("D\\Рабочий стол\\Саша.xml", FileMode.OpenOrCreate))
+    {
+        xml.Serialize(fs, Sasha);
+    }
+}
+
---
+++ XMl2.cs
@@ -0,0 +1,23 @@
+﻿using СП6;
+using System.Xml.Serialization;
+
+
+List<Student> students;
+
+Console.WriteLine("Файл: ");
+string path = Console.ReadLine();
+
+if (path.EndsWith(" .xml"))
+{
+    XmlSerializer xml = new XmlSerializer(typeof(Student));
+    using (FileStream fs = new FileStream("Саша.xml", FileMode.OpenOrCreate)) ;
+    {
+        student = (List<Student>)xml.Deserialize(fs);
+    }
+}
+
+XmlSerializer xml = new XmlSerializer(typeof(Student));
+using (FileStream fs = new FileStream("D\\Рабочий стол\\Саша.xml", FileMode.OpenOrCreate))
+{
+    xml.Serialize(fs, Sasha);
+}
