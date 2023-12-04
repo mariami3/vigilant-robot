07d834e875e65f20726afe5d99c6e73f1d1ad276
mariami3 <144897983+mariami3@users.noreply.github.com>
2023-12-04 22:58:02 +0300

    Add files via upload

---
+++ ClassUser8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР8
+{
+    [Serializable]
+    internal class ClassUser
+    {
+        public string? Name;
+        public double РекорднаяСкоростьВводаСимволовВсекунду;
+        public double РекорднаяСкоростьВводаСимволовВминуту;
+    }
+}
