c5ef0fbaa36b50e6afa4f4d695f9b4d95bc38878
mariami3 <144897983+mariami3@users.noreply.github.com>
2024-03-16 21:54:01 +0300

    Add files via upload

---
+++ PY1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PY1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ComicsShopEntities context = new ComicsShopEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            comics сomicsWindow = new comics();
+            сomicsWindow.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            genres genresWindow = new genres();
+            genresWindow.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            delevery authorWindow = new delevery();
+            authorWindow.ShowDialog();
+        }
+
+
+    }
+}
---
+++ PY1/delevery.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PY1
+{
+    /// <summary>
+    /// Логика взаимодействия для delevery.xaml
+    /// </summary>
+    public partial class delevery : Window
+    {
+        private ComicsShopEntities context = new ComicsShopEntities();
+
+        public delevery()
+        {
+            InitializeComponent();
+            Delevery.ItemsSource = context.Delevery.ToList();
+
+        }
+
+
+        private void Delevery_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
