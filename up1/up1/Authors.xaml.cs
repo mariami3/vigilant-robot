76a74a0a825c32d5ef41536c32c4a01485ee4ca5
mariami3 <144897983+mariami3@users.noreply.github.com>
2024-03-16 21:05:20 +0300

    Add files via upload

---
+++ up1/up1/Authors.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using up1.ComicsStoreDataSetTableAdapters;
+
+namespace up1
+{
+    /// <summary>
+    /// Логика взаимодействия для Authors.xaml
+    /// </summary>
+    public partial class Authors : Window
+    {
+        AuthorsTableAdapter authors = new AuthorsTableAdapter();
+
+        public Authors()
+        {
+            InitializeComponent();
+            AuthorsDataGrid.ItemsSource = authors.GetData();
+
+        }
+
+
+    }
+}
---
+++ up1/up1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using up1.ComicsStoreDataSetTableAdapters;
+
+
+
+
+
+namespace up1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComicsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Comics сomicsWindow = new Comics();
+            сomicsWindow.ShowDialog();
+        }
+
+        private void AuthorsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Authors authorWindow = new Authors();
+            authorWindow.ShowDialog();
+        }
+
+        private void GenresButton_Click(object sender, RoutedEventArgs e)
+        {
+            Genres genresWindow = new Genres();
+            genresWindow.ShowDialog();
+        }
+    }
+}
+
+
